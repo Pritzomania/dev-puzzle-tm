c9eb935f877f95c485a921ad7d6aa0ec3f57c0d0
Prithwiraj Sinha <prithwiraj.sinha.dev@gmail.com>
2020-05-14 16:43:05 +0530

    Fixed tests and updated gitignore file to exclude coverage reports

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,4 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { StoreModule } from '@ngrx/store';
 
 import { StocksComponent } from './stocks.component';
 
@@ -8,9 +20,21 @@ describe('StocksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
-    })
-    .compileComponents();
+      declarations: [StocksComponent],
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        SharedUiChartModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        StoreModule.forRoot({})
+      ],
+      providers: [PriceQueryFacade]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
