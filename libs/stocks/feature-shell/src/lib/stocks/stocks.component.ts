e32f71bccbd62728f3fbca25c1a72bbbcfb9d975
Prithwiraj Sinha <prithwiraj.sinha.dev@gmail.com>
2020-05-15 12:53:05 +0530

    removed the go button and implemented automatic refresh with debouncing

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
-  symbol: string;
-  period: string;
-
   quotes$ = this.priceQuery.priceQueries$;
+  formSub$: Subscription;
 
   timePeriods = [
     { viewValue: 'All available data', value: 'max' },
@@ -32,7 +32,15 @@ export class StocksComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.formSub$ = this.stockPickerForm.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(() => this.fetchQuote());
+  }
+
+  ngOnDestroy(): void {
+    this.formSub$.unsubscribe();
+  }
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
